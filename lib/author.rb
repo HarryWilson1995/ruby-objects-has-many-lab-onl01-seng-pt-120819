ccdbf81d507810b07ace05b0128a077a63a24089
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-13 15:20:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,25 @@
-class Artist 
+class Author  
   attr_accessor :name 
   
   def initialize(name)
     @name = name 
   end 
   
-  def songs 
-    Song.all.select {|song| song.artist == self}
+  def posts
+    Post.all.select {|posts| posts.author == self}
   end 
   
-  def add_song(song) 
-    song.artist = self 
+  def add_post(post) 
+    post.author = self 
   end 
   
-  def add_song_by_name(name) 
+  def add_post_by_title(post) 
     title = Song.new(name)
-    add_song(title)
+    add_post(title)
   end 
   
-  def self.song_count
-    Song.all.count 
+  def self.post_count
+    Post.all.count 
   end 
   
 end 
